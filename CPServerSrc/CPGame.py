1e684d37fb6bb29584dd9f877f8abf2eafa3de3a
mattcushman <matt.cushman@etale.com>
2021-10-03 19:57:56 -0400

    try changing game state to POST

--- CPServerSrc/CPGame.py
+++ CPServerSrc/CPGame.py
@@ -25,9 +25,7 @@ def computeMoveSignature(hand):
         sig.append(0)
     if len(hand) == 5 and ( hand[0] // 4 == hand[1] // 4 and hand[1] // 4 == hand[2] // 4 and hand[3] // 4 == hand[4] // 4):
         sig.append(1+hand[2])
-    else:
-        sig.append(0)
-    if len(hand) == 5 and ( hand[0] // 4 == hand[1] // 4 and hand[2] // 4 == hand[3] // 4 and hand[3] // 4 == hand[4] // 4):
+    elif len(hand) == 5 and ( hand[0] // 4 == hand[1] // 4 and hand[2] // 4 == hand[3] // 4 and hand[3] // 4 == hand[4] // 4):
         sig.append(1+hand[-1])
     else:
         sig.append(0)
